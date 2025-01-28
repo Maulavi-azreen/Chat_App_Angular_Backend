0e4c50ca6987e518eb32d938cc3a3dd4ac1fbce5
Azreen Maulavi <Azreen.Maulavi@facileserv.local>
2025-01-28 15:47:19 +0530

    updated db.js and index.js

--- config/db.js
+++ config/db.js
@@ -10,15 +10,17 @@ console.log("MONGO_URI:", process.env.MONGO_URI);
 const { MONGO_URI } = process.env;
 
 const connectDB = () => {
+  const dbName = "Chat_App";
   mongoose
     .connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 10000, // Wait for 10 seconds before timing out
       socketTimeoutMS: 45000, // 45 seconds
+      dbName:"Chat_App"
     })
     .then(() => {
-      console.log("Successfully connected to MongoDB");
+      console.log("Successfully connected to MongoDB and DB Name is",dbName);
     })
     .catch((err) => {
       console.error("Error connecting to MongoDB:", err);
